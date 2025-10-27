df798ada4b51ccc8b05c3932f697fb6442bd3cc4
buri <brian@urizone.net>
2025-10-27 13:32:19 -0400

    e1q01p2 solution.

--- src/buri/ec/e1/q01/Puzzle.kt
+++ src/buri/ec/e1/q01/Puzzle.kt
@@ -3,7 +3,9 @@ package buri.ec.e1.q01
 import buri.ec.common.BasePuzzle
 import buri.ec.common.Part
 import buri.ec.common.extractInts
+import buri.ec.common.extractLongs
 import org.junit.Test
+import java.math.BigInteger
 import kotlin.math.pow
 
 /**
@@ -22,7 +24,7 @@ class Puzzle : BasePuzzle() {
     @Test
     fun runPart2() {
         assertRun(11051340, true)
-        assertRun(0, false, true)
+        assertRun(236309384189337, false, true)
     }
 
     @Test
@@ -37,7 +39,7 @@ class Puzzle : BasePuzzle() {
     override fun run(part: Part, input: List<String>): Number {
         var max = 0L
         for (line in input) {
-            val numbers = line.extractInts()
+            val numbers = line.extractLongs()
             val result =
                 eni(part, numbers[0], numbers[3], numbers[6]) +
                         eni(part, numbers[1], numbers[4], numbers[6]) +
@@ -47,12 +49,20 @@ class Puzzle : BasePuzzle() {
         return max
     }
 
-    private fun eni(part: Part, n: Int, exp: Int, mod: Int): Long {
-        var remainders = mutableListOf<Int>()
-        var score = 1
-        repeat(exp) {
-            score = score * n % mod
-            remainders.add(0, score)
+    private fun eni(part: Part, n: Long, exp: Long, mod: Long): Long {
+        var remainders = mutableListOf<Long>()
+        if (part.isOne()) {
+            var score = 1L
+            repeat(exp.toInt()) {
+                score = score * n % mod
+                remainders.add(0, score)
+            }
+        } else {
+            for (i in 0..4) {
+                val expStep = BigInteger.valueOf(exp - i)
+                val result = BigInteger.valueOf(n).modPow(expStep, BigInteger.valueOf(mod))
+                remainders.add(result.toLong())
+            }
         }
         return remainders.joinToString("").toLong()
     }
