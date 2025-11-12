9368073b3af881bfbdd8ec80c3fc87d3f0a97818
buri <brian@urizone.net>
2025-11-12 10:46:11 -0500

    e2q02 solution.

---
+++ src/buri/ec/e2/q02/Puzzle.kt
@@ -0,0 +1,76 @@
+package buri.ec.e2.q02
+
+import buri.ec.common.BasePuzzle
+import buri.ec.common.Part
+import org.junit.Test
+
+/**
+ * Entry point for a daily puzzle
+ *
+ * @author Brian Uri!
+ */
+class Puzzle : BasePuzzle() {
+
+    @Test
+    fun runPart1() {
+        assertRun(7, true)
+        assertRun(128, false, true)
+    }
+
+    @Test
+    fun runPart2() {
+        assertRun(2955, true)
+        assertRun(21570, false, true)
+    }
+
+    @Test
+    fun runPart3() {
+        assertRun(21550686, false, true)
+    }
+
+    /**
+     * Executes a part of the puzzle using the specified input.
+     */
+    override fun run(part: Part, input: List<String>): Number {
+        var bolts = 0
+        val boltColors = "RGB"
+        if (part.isOne()) {
+            var balloons = input[0]
+            while (balloons.isNotEmpty()) {
+                val bolt = boltColors[bolts % boltColors.length]
+                while (balloons.startsWith(bolt)) {
+                    balloons = balloons.drop(1)
+                }
+                balloons = balloons.drop(1)
+                bolts++
+            }
+        } else {
+            val repeats = if (part.isTwo()) {
+                100
+            } else {
+                100000
+            }
+            // Use two deques. The point between them is the balloon across the circle.
+            val balloons = input[0].repeat(repeats)
+            val balloonsBefore = ArrayDeque<Char>()
+            val balloonsAfter = ArrayDeque<Char>()
+            balloonsBefore.addAll(balloons.take(balloons.length / 2).toCharArray().toList())
+            balloonsAfter.addAll(balloons.takeLast(balloons.length / 2).toCharArray().toList())
+
+            while (balloonsBefore.size + balloonsAfter.size > 0) {
+                val bolt = boltColors[bolts % boltColors.length]
+                val isEven = ((balloonsBefore.size + balloonsAfter.size) % 2 == 0)
+                val matchesFirstBalloon = (bolt == balloonsBefore.first())
+                if (matchesFirstBalloon && isEven) {
+                    balloonsAfter.removeFirst()
+                }
+                balloonsBefore.removeFirst()
+                if (balloonsAfter.size > balloonsBefore.size) {
+                    balloonsBefore.add(balloonsAfter.removeFirst())
+                }
+                bolts++
+            }
+        }
+        return bolts
+    }
+}
